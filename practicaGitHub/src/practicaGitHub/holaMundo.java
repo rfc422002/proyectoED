333345e074de2e4a3076e284d0e91747bcae5ef7
windows10 <windows10@DESKTOP-B8E4IF5>
2021-05-25 07:29:51 +0200

    Actualizamos la conexion

--- practicaGitHub/src/practicaGitHub/holaMundo.java
+++ practicaGitHub/src/practicaGitHub/holaMundo.java
@@ -3,5 +3,9 @@ package practicaGitHub;
 public class holaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		
+		System.out.println("Ya se ha compartido el proyecto");
+		
+		
 	}
 }
